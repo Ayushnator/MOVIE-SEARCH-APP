a12bd4cb83b3a63fc3c4f05508f48f1c27eecbee
Ayush Singh <158266574+Ayushnator@users.noreply.github.com>
2025-07-11 16:17:29 +0530

    Update api key and url for vercel

--- src/About.jsx
+++ src/About.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom'; // <-- Add this
+    const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+    const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
